55b87fef047dc165bb5634fedaa5738eb6064abe
Leon <schneiderjohnleon@gmail.com>
2026-01-15 10:35:07 +0100

    first commit

---
+++ helloworld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"This is line number {i}");
+
+        }
+
+        int h = 0;
+
+        while(h < 50)
+        {
+            h++;
+            if (h % 2 != 0)
+            {
+                Console.WriteLine(h + "     Zahl ungrade");
+            } else
+            {
+                Console.WriteLine(h);
+            }
+        } 
+
+        
+    }
+}
+
+
+
